c98edfd73d5f785622f6eb613cdfc45ea636f7b0
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2022-01-01 23:37:27 +0100

    Express

--- TestApp/Backend/TestRoute/Route_Test.js
+++ TestApp/Backend/TestRoute/Route_Test.js
@@ -3,7 +3,9 @@ const express = require('../../../index').Express
 const router = express.Router()
 
 router.get("/", (req, res) => {
-    res.send({Valide: true, Message:"route de test"})
+    //res.send({Valide: true, Message:"route de test"})
+    const path = require('path');
+    res.sendFile(path.join(__dirname, '/TestHtmlPage.html'));
 })
 
 module.exports = router
